fa4feea9c78ba0005e2f70e6e1d68399df77134d
murrayn808 <murrayn@hawaii.edu>
2022-05-13 10:45:40 -1000

    Rectangle class working

--- main.cpp
+++ main.cpp
@@ -1,11 +1,72 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_final_exam - EE 205 - Spr 2022
+///
+/// @file actualfile.cpp
+/// @version 1.0
+///
+/// @author <murrayn@hawaii.edu>
+/// @date   13_May_2022
+///////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 
-int main() {
-   std::cout << "Hello, University of Hawaii, College of Engineering!" << std::endl;
-   std::cout << "Final Exam - EE 205 - Spr 2022" << std::endl;
-   std::cout << std::endl;
-   std::cout << "My name is: NATHANIEL MURRAY" << std::endl;
-   std::cout << "A fun fact about me is I LOVE CATS SO MUCH" << std::endl;
+class Shape{
+public:
+    virtual double compute_area() = 0;
+};
 
-   return 0;
+class Rectangle : public Shape {
+public:
+
+    virtual double compute_area(){
+        return length*width;
+    }
+    Rectangle(double newLength, double newWidth){
+        if(newLength<0 || newWidth<0){
+            throw std::invalid_argument("Length must be > 0.");
+        }
+        length = newLength;
+        width = newWidth;
+    };
+
+    double getLength() const noexcept{
+        return length;
+    }
+
+    double getWidth() const noexcept{
+        return width;
+    }
+
+
+protected:
+    double length;
+    double width;
+};
+
+class Square : public Rectangle, public Shape {
+public:
+    double getSide(){
+        return length;
+    }
+
+    /*Square(double newSide) : Rectangle(double newLength, double newWidth), Shape (){
+        newLength = newSide;
+    }
+     */
+};
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    /*Square mySquare(2);
+    std::cout << "The area of mySquare is: "
+              << mySquare.compute_area()
+              << std::endl;
+              */
+
+    Rectangle myRectangle( 2, 4 );
+    std::cout << "The area of myRectangle is: "
+              << myRectangle.compute_area()
+              << std::endl;
+
+    return 0;
 }
