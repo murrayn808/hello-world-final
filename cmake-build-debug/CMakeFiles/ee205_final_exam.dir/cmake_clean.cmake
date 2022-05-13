ded0cc708cd27e76fdb1221c3efbd677e4d62723
murrayn808 <murrayn@hawaii.edu>
2022-05-12 14:34:48 -1000

    Fixed mapping

---
+++ cmake-build-debug/CMakeFiles/ee205_final_exam.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ee205_final_exam.dir/main.cpp.o"
+  "CMakeFiles/ee205_final_exam.dir/main.cpp.o.d"
+  "ee205_final_exam"
+  "ee205_final_exam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ee205_final_exam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ee205_final_exam.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ee205_final_exam.
+# This may be replaced when dependencies are built.
