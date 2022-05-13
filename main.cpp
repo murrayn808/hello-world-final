f9353408421a4f57e345c7a68ed82d9dfbf10b08
murrayn808 <murrayn@hawaii.edu>
2022-05-12 14:13:03 -1000

    Hello world message

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 
 int main() {
    std::cout << "Hello, University of Hawaii, College of Engineering!" << std::endl;
-   std::cout << "Lab 06d - Hello CLion - EE 205 - Spr 2022" << std::endl;
+   std::cout << "Final Exam - EE 205 - Spr 2022" << std::endl;
    std::cout << std::endl;
-   std::cout << "My name is:  <<NATHANIEL MURRAY>>" << std::endl;
-   std::cout << "A fun fact about me is <<I LOVE CATS SO MUCH>>" << std::endl;
+   std::cout << "My name is: NATHANIEL MURRAY" << std::endl;
+   std::cout << "A fun fact about me is I LOVE CATS SO MUCH" << std::endl;
 
    return 0;
 }
