1d7fc4092138f3cb1b7e566d07280bd390f5411b
murrayn808 <murrayn@hawaii.edu>
2022-05-13 10:50:33 -1000

    Square class almost working

--- main.cpp
+++ main.cpp
@@ -49,19 +49,19 @@ public:
         return length;
     }
 
-    /*Square(double newSide) : Rectangle(double newLength, double newWidth), Shape (){
-        newLength = newSide;
+    Square(double newLength, double newWidth, double newSide) : Rectangle(newLength, newWidth) {
+        length = newSide;
     }
-     */
+
 };
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    /*Square mySquare(2);
+    Square mySquare(2);
     std::cout << "The area of mySquare is: "
               << mySquare.compute_area()
               << std::endl;
-              */
+
 
     Rectangle myRectangle( 2, 4 );
     std::cout << "The area of myRectangle is: "
